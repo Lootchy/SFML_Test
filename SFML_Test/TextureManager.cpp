509f9c8dd6c43ca15fbe2f392139ae744dab13e0
Lootchy <zcolucci@gaming.tech>
2025-03-16 12:15:48 +0100

    Update TextureManager.cpp

--- SFML_Test/TextureManager.cpp
+++ SFML_Test/TextureManager.cpp
@@ -7,16 +7,16 @@ TextureManager::TextureManager()
 map<string, sf::Texture*> TextureManager::textures;
 std::vector<string> TextureManager::order;
 
-// Get Length of Textures Array
+
 int TextureManager::getLength() {
     return textures.size();
 }
 
-// Get Texture by Name
+
 sf::Texture* TextureManager::getTexture(string name)
 {
 
-    // See if we have already loaded this texture
+
     if (textures.find(name) != textures.end()) {
         return textures[name];
     }
@@ -25,30 +25,30 @@ sf::Texture* TextureManager::getTexture(string name)
     }
 }
 
-// Get Texture by Index
+
 sf::Texture* TextureManager::getTexture(int index)
 {
-    // Stay DRY and reuse get by name, but get string name from vector with index
+    
     return getTexture(order.at(index));
 }
 
-// Assign a Texture a Name (for accessing via get) and path (to load from)
+
 sf::Texture* TextureManager::loadTexture(string name, string path)
 {
-    // Haven't loaded it yet, time to create it
+    
     sf::Texture* texture = new sf::Texture();
 
     if (texture->loadFromFile(path))
     {
         textures[name] = texture;
 
-        // Push to vector the order in which items were loaded into map, for accessing via index.
+        
         order.push_back(name);
         return textures[name];
     }
     else
     {
-        // Could not load the file
+
         delete texture;
         return NULL;
     }
@@ -58,7 +58,7 @@ sf::Texture* TextureManager::loadTexture(string name, string path)
 TextureManager::~TextureManager()
 {
 
-    // Delete all of the textures we used
+    
     sf::Texture* texture;
     map<string, sf::Texture*>::iterator iter = textures.begin();
     while (iter != textures.end())
